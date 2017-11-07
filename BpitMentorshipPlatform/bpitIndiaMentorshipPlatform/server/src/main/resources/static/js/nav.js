26617d104ee70574348fc918750ddbbf8a25ef63
Ruchit <ruchit.jain15@gmail.com>
2017-11-07 23:47:19 +0530

    Author : Ruchit Jain

--- BpitMentorshipPlatform/bpitIndiaMentorshipPlatform/server/src/main/resources/static/js/nav.js
+++ BpitMentorshipPlatform/bpitIndiaMentorshipPlatform/server/src/main/resources/static/js/nav.js
@@ -1,16 +1,20 @@
 // Controller for Home Page
-app.controller('navController', function($scope, $rootScope) {
-    $scope.logoutToggle = false;
-    $rootScope.manageToggle = false;
+app.controller('navController', function($scope, $http, $rootScope) {
+    //$scope.logoutToggle = false;
+    //$rootScope.manageToggle = false;
 	
 	// aap bs mujhe vha pocha do jha pe login succes ho rha hok
-    $scope.navModel = "";
+    //$scope.navModel = "";
     console.log("in nav ctrl");
-    console.log($rootScope.logoutToggle);
+    //console.log($rootScope.logoutToggle);
 
     $scope.logoutFunction = function(){
+    	console.log("In logout");
         localStorage.removeItem('user');
+        console.log("user is :" +localStorage.getItem('user'));
         localStorage.removeItem('userRole');
         localStorage.removeItem('userCourse');
+        $scope.logoutToggle = false;
+        $rootScope.manageToggle = false;
     }
 });
